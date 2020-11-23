4d14cf4d03424d019e627e29c08ee1050fb37151
avs <antoine.van.serveyt@avanserv.com>
2020-11-23 21:14:13 +0100

    [ADD] project: basic setup and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn'
+  }
+};
