11289e9db96a07eb192e763d0f2366c3f89dd311
avs <antoine.van.serveyt@avanserv.com>
2020-12-01 11:29:13 +0100

    [IMP] project: enable eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
-module.exports = {
+export default {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true
   },
-  extends: [],
+  extends: ['@skyra/eslint-config'],
   parserOptions: {
-    ecmaVersion: 2020
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module'
   },
   rules: {
     'no-console': 'warn',
-    'no-debugger': 'warn'
-  }
+    'no-debugger': 'warn',
+    'no-throw-literal': 0,
+    '@typescript-eslint/restrict-plus-operands': 0,
+    '@typescript-eslint/indent': ['error', 2]
+  },
+  'overrides': [
+    {
+      'files': [
+        'src/**/*.ts'
+      ],
+      'rules': {
+        '@typescript-eslint/require-await': 0
+      }
+    }
+  ]
 };
