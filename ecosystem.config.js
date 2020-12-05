03b77af9111b54cbc2f49123b00ba982eec74ff2
avs <antoine.van.serveyt@avanserv.com>
2020-12-05 10:36:03 +0100

    [ADD] project: pm2 process management

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [
+    {
+      name: 'kbot',
+      script: './dist/client.js',
+      watch: './dist',
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: '250M',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
