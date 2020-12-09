56b5804efc9c055411066baa52f98793e04fb8ec
avs <antoine.van.serveyt@avanserv.com>
2020-12-09 09:59:58 +0100

    [IMP] tasks: almanax
    Send the almanax for the next day as the hosting platform is not
    in the same timezone as the Dofus servers.

--- src/tasks/almanax.ts
+++ src/tasks/almanax.ts
@@ -25,7 +25,7 @@ export default class AlmanaxTask extends Task {
       if (!(channel instanceof TextChannel)) continue;
 
       const message = dummyMessage(this.client, channel);
-      command.exec(message, { extended: true });
+      command.exec(message, { extended: false, target: '+1' });
     }
   }
 
