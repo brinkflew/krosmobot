438f99f0fcf363d6569a68174b3de7528a6e9248
avs <antoine.van.serveyt@avanserv.com>
2021-01-13 19:45:33 +0100

    [REM] constants: unneccessary Twitter-related constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,6 @@
 
 import defaults from './defaults';
 import * as embeds from './embeds';
-import * as twitter from './twitter';
 
 // Defaults
 export const DEFAULT_PREFIX = defaults.PREFIX;
@@ -19,6 +18,3 @@ export const EMBED_COLOR_MAGENTA = embeds.COLORS.MAGENTA;
 export const EMBED_COLOR_RED = embeds.COLORS.RED;
 export const EMBED_COLOR_YELLOW = embeds.COLORS.YELLOW;
 export const EMBED_COLOR_DEFAULT = embeds.COLORS.DEFAULT;
-
-// Twitter
-export const TWITTER_USERS = twitter.users;
--- src/constants/twitter.ts
+++
@@ -1,8 +0,0 @@
-export const users = [
-  '72272795',             // DOFUSfr
-  '39510851',             // AnkamaGames
-  '948118597095837696',   // AnkamaLive
-  '1348954685810614276'   // Krosmobot
-];
-
-export default users;
