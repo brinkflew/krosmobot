5288d54812c1e6b736b99f74b9b41c2eeb83fe9d
avs <antoine.van.serveyt@avanserv.com>
2021-01-23 13:17:07 +0100

    [ADD] tasks: prune-logs
    Remove logs older than 15 days from the database and clear the cache of logs to
    free up memory.

---
+++ src/tasks/prune-logs.ts
@@ -0,0 +1,25 @@
+import { Task } from '@/structures';
+
+/**
+ * Deletes logs older than 15 days in the database and
+ * clears the cache of saved logs since the process started to free up memory.
+ */
+export default class LogsPruneTask extends Task {
+
+  public constructor() {
+    super('prune-logs', { at: '03:00' });
+  }
+
+  /**
+   * Runs the task.
+   */
+  public async exec() {
+    this.client.logs.items.clear();
+
+    const delta = Date.now() - (1000 * 60 * 60 * 24 * 15);
+    const result = await this.client.logs.model.deleteMany({ timestamp: { $lt: delta } });
+    if (!result.deletedCount) return;
+    return { deleted: `${result.deletedCount || 0} records` };
+  }
+
+}
