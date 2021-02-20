1c1b0e36f7d886c0f615070cf5633f9de8501a3b
avs <antoine.van.serveyt@avanserv.com>
2021-02-20 02:53:12 +0100

    [ADD] arguments: online
    Keep the first line of the content after splitting
    it on carriage returns.

--- src/arguments/index.ts
+++ src/arguments/index.ts
@@ -3,11 +3,13 @@ import { dofusJob } from './dofus/job';
 import { dofusServer } from './dofus/server';
 import { duration } from './time/duration';
 import { line } from './text/line';
+import { oneline } from './text/oneline';
 
 export const argumentTypes = {
   dofusDimension,
   dofusJob,
   dofusServer,
   duration,
-  line
+  line,
+  oneline
 };
