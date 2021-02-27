1de78b79ca7a2c5eee2441c6a59f616a69479583
avs <antoine.van.serveyt@avanserv.com>
2021-02-27 11:25:21 +0100

    [IMP] commands: zaap
    Match coordinates containing whitespaces.

--- src/arguments/dofus/coordinates.ts
+++ src/arguments/dofus/coordinates.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js';
 
 export const dofusCoordinates = (_message: Message, phrase: string) => {
   if (!phrase) return null;
-  const parsed = /(-?[0-9]{1,3}).(-?[0-9]{1,3})/i.exec(phrase);
+  const parsed = /(-?[0-9]{1,3})[\s.;,:|]+(-?[0-9]{1,3})/i.exec(phrase);
   if (!parsed) return null;
   return { x: parseInt(parsed[1], 10), y: parseInt(parsed[2], 10), world: 'main' };
 };
